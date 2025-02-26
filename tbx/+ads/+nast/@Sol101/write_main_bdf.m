b585c934909d6523a01f901ec7c3e01eb6d23e7d
Fintan Healy <fintan.healy@bristol.ac.uk>
2025-02-26 10:56:54 +0000

    added option for extra case control commands

--- tbx/+ads/+nast/@Sol101/write_main_bdf.m
+++ tbx/+ads/+nast/@Sol101/write_main_bdf.m
@@ -12,6 +12,7 @@ mni.printing.bdf.writeHeading(fid,'Case Control');
 mni.printing.bdf.writeColumnDelimiter(fid,'8');
 println(fid,'NASTRAN NLINES=999999');
 println(fid,'SOL 101');
+
 println(fid,'CEND');
 mni.printing.bdf.writeHeading(fid,'Case Control')
 println(fid,'ECHO=NONE');
@@ -21,6 +22,12 @@ fprintf(fid,'LOAD=%.0f\n',obj.Load_ID);
 println(fid,'DISPLACEMENT(SORT1,REAL)=ALL');
 println(fid,'FORCE(SORT1,REAL)=ALL');
 println(fid,'GROUNDCHECK=YES');
+% extra case control lines
+if ~isempty(obj.ExtraCaseControl)
+    for i = 1:length(obj.ExtraCaseControl)
+        println(fid,obj.ExtraCaseControl(i));
+    end
+end
 mni.printing.bdf.writeHeading(fid,'Begin Bulk')
 %% Bulk Data
 println(fid,'BEGIN BULK')
@@ -39,7 +46,13 @@ mni.printing.cards.PARAM('BAILOUT','i',-1).writeToFile(fid);
 mni.printing.cards.PARAM('OPPHIPA','i',1).writeToFile(fid);
 mni.printing.cards.PARAM('AUNITS','r',0.1019716).writeToFile(fid);
 mni.printing.cards.MDLPRM('HDF5','i',0).writeToFile(fid);
-
+%extra cards
+if ~isempty(obj.ExtraCards)
+    mni.printing.bdf.writeComment(fid,'Extra Cards')
+end
+for i = 1:length(obj.ExtraCards)
+    obj.ExtraCards(i).writeToFile(fid);
+end
 
 %write Boundary Conditions
 mni.printing.bdf.writeComment(fid, 'SPCs')
